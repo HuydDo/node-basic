fd63a7ae37332cd14615163a6c16f22184a9a289
HuydDo <huydinhdo@yahoo.com>
2020-11-01 19:17:03 -0700

    install express-validator, modified package.json, post controllers

--- node_react/nodeapi/controllers/post.js
+++ node_react/nodeapi/controllers/post.js
@@ -1,4 +1,4 @@
-const Post = require('../models/post');
+const Post = require("../models/post");
 
 exports.getPosts = (req, res) => {
 
@@ -7,16 +7,20 @@ exports.getPosts = (req, res) => {
   res.json({
     posts: [{title: 'First post'},{title: 'Second post'}
     ]
-  })
-
-}
+  });
+};
 
 exports.createPost = (req, res) => {
   const post = new Post(req.body);
   // console.log("CREATING POST: ", req.body);
   post.save((err, result) => {
     if(err){
-      return res.status()
+      return res.status(400).json({
+        error: err
+      })
     }
+    res.status(200).json({
+      post: result
+    })
   })
 }
